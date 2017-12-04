50cae64e63a0e9d96e6935b0e7c86f5a97d66c0c
blockchain99 <shinebytheriver@yahoo.com>
2017-12-04 15:37:57 -0800

    map(x => (x, 1).reduceBykey(_+_)

--- src/main/scala/com/sparkTutorial/rdd/reduce/ReduceExampleSession.scala
+++ src/main/scala/com/sparkTutorial/rdd/reduce/ReduceExampleSession.scala
@@ -1,5 +1,6 @@
 package com.sparkTutorial.rdd.reduce
 
+import com.sparkTutorial.commons.Utils
 import org.apache.spark.sql.SparkSession
 
 object ReduceExampleSession {
@@ -10,20 +11,22 @@ object ReduceExampleSession {
       .getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
 
-    val inputInteger = (1 to 5).toList
-    val integerRdd = spark.sparkContext.parallelize(inputInteger)
+    val inputInteger = (1 to 5).toList  //List[Int]
+    val integerRdd = spark.sparkContext.parallelize(inputInteger)  //RDD[Int]
 
     val product = integerRdd.reduce((x,y) => x*y)
     println("product : "+product)
     println("*******************************")
     val inputString = List("This", "That", "Test", "This", "Exercise")
     val inputStringRdd = spark.sparkContext.parallelize(inputString)
-    val result = inputStringRdd.flatMap(list => list.split(" "))
+    val result = inputStringRdd.flatMap(list => list.split(Utils.COMMA_DELIMITER))
+   //     .filter(word => !word.isEmpty)
       .map(word => (word, 1))
       .reduceByKey(_+_)
     println("result : "+result)
     println(result.foreach(println))
     //.collect()  //convert RDD to Array
+
 }
 
 }
