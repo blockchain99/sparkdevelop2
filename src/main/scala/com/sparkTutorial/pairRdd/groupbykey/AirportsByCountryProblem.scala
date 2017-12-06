0c55db896a4663ada5a384dc3f2362ffffef2cdf
blockchain99 <shinebytheriver@yahoo.com>
2017-12-05 17:44:59 -0800

    map(x => (x, 1).reduceBykey(_+_)

--- src/main/scala/com/sparkTutorial/pairRdd/groupbykey/AirportsByCountryProblem.scala
+++ src/main/scala/com/sparkTutorial/pairRdd/groupbykey/AirportsByCountryProblem.scala
@@ -1,11 +1,14 @@
 package com.sparkTutorial.pairRdd.groupbykey
 
+import com.sparkTutorial.commons.Utils
+import org.apache.spark.sql.SparkSession
+
 object AirportsByCountryProblem {
 
   def main(args: Array[String]) {
 
     /* Create a Spark program to read the airport data from in/airports.text,
-       output the the list of the names of the airports located in each country.
+       output the the list of the names of the airports(value) located in each country(key).
 
        Each row of the input file contains the following columns:
        Airport ID, Name of airport, Main city served by airport, Country where airport is located, IATA/FAA code,
@@ -18,5 +21,17 @@ object AirportsByCountryProblem {
        "Papua New Guinea",  List("Goroka", "Madang", ...)
        ...
      */
+    val spark = SparkSession.builder()
+      .appName("upperCase")
+      .master("local")
+      .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+    val airportsRDD = spark.sparkContext.textFile("in\\airports.text")  //head column without feature name
+    //(country, airportName ) pair
+    val pairRdd = airportsRDD.map((line: String) => (line.split(Utils.COMMA_DELIMITER)(3), line.split(Utils.COMMA_DELIMITER)(1)) )
+    val airportsByCountry = pairRdd.groupByKey()
+    airportsByCountry.first()
+//    for((country, airport) <- airportsByCountry.collect()) println(country +" : "+airport) //CompactBuffer(airport list)
+    for((country, airport) <- airportsByCountry.collectAsMap()) println(country +" : "+airport.toList) //List output for airportName
   }
 }
