1cdcf08910e48f5f2d707d79981e566ac18fc4c6
blockchain99 <shinebytheriver@yahoo.com>
2017-12-05 17:10:07 -0800

    map(x => (x, 1).reduceBykey(_+_)

--- src/main/scala/com/sparkTutorial/pairRdd/aggregation/reducebykey/housePrice/AverageHousePriceProblem.scala
+++ src/main/scala/com/sparkTutorial/pairRdd/aggregation/reducebykey/housePrice/AverageHousePriceProblem.scala
@@ -1,5 +1,8 @@
 package com.sparkTutorial.pairRdd.aggregation.reducebykey.housePrice
 
+import com.sparkTutorial.commons.Utils
+import org.apache.spark.sql.SparkSession
+
 object AverageHousePriceProblem {
 
   def main(args: Array[String]) {
@@ -26,13 +29,61 @@ object AverageHousePriceProblem {
 
     Sample output:
 
-       (3, 325000)
+       (3, 325000)  //(numberOfBedroom, avgPrice)
        (1, 266356)
        (2, 325000)
        ...
 
        3, 1 and 2 mean the number of bedrooms. 325000 means the average price of houses with 3 bedrooms is 325000.
      */
+    val spark = SparkSession.builder()
+      .appName("bathRoomPrice")
+      .master("local")
+      .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+    val houseRdd = spark.sparkContext.textFile("in/RealEstate.csv")
+    val firstLine = houseRdd.first()
+    val pureRdd = houseRdd.filter(line => line != firstLine)
+    println("pureRdd.first() :" + pureRdd.first())
+
+    //    val pairRdd = pureRdd.map(line => (line.split(Utils.COMMA_DELIMITER)(3),
+    //      line.split(Utils.COMMA_DELIMITER)(2)))
+
+    /*option1 Rdd.map(p => (p.makeKey, p.makeValue))
+    using pariRdd.groupByKey() in which pairRdd consists of (key, value)*/
+    val pairRdd = pureRdd.map(line => (line.split(Utils.COMMA_DELIMITER)(3),
+      line.split(Utils.COMMA_DELIMITER)(2).toDouble))   //(bedRoomNumber, housePrice)
+    println("** pairRdd.first() :" + pairRdd.first())
+    //    val bedroomAvgPriceRdd = pairRdd.flatMap()
+    println("-pairRdd.count() :" + pairRdd.count())
+    /* making RDD with key, value pair is omitted   */
+    val interimResult = pairRdd.groupByKey() //RDD[Key, Iterable[Value]
+      .map(p => (p._1, (p._2.size, p._2.sum))) // key(bedroom), (total number for the bedroom, sum of the bedroom
+    println("* interimResult.first(): ")
+    println(interimResult.first())
+    println("---------final result--------------------")
+    val finalResult = pairRdd.groupByKey()
+      .map(p => (p._1, p._2.sum / p._2.size))
+     //   .collect()  //convert from RDD to Array
+    finalResult.foreach(println)
+    println("================ option2 using reducByKey ==============")
+    /* optoin2 Rdd.map(p => (p.bedroomNumber, (1, p.price))
+    using reduceByKey((v1,v2) => ((v1._1 + v2._1),(v1._2 + v2._2))  :
+                             (v1._1 + v2._1): freuency of specific bedroom number:(1+1+1...)
+                              (v1._2 + v2._2): sum of specific bedroom number*/
+    val pairRdd2 = pureRdd.map(line => (line.split(Utils.COMMA_DELIMITER)(3),
+      (1, line.split(Utils.COMMA_DELIMITER)(2).toDouble)))
+    /* (bedroomNumber, (1, housrPrice))  : (key, (value._1, value._2))   */
+    println(pairRdd2)
+    /* reduceByKey is only for value */
+    val interimRdd2 = pairRdd2.reduceByKey((v1, v2) =>( (v1._1 + v2._1), (v1._2 + v2._2) ) )
+    println("interimRdd2.first() : "+ interimRdd2.first())
+//    val finalRdd2 = pairRdd2.reduceByKey((v1, v2)=> ( (v1._2 + v2._2) / (v1._1+v2._1) ) )  //error !
+    val finalRdd2 = interimRdd2.map(p => (p._1,(p._2._2/p._2._1)) )
+    println(finalRdd2.first())
+    println("*** ( bedromm number , average price)****")
+    finalRdd2.foreach(println)
+
   }
 
 }
