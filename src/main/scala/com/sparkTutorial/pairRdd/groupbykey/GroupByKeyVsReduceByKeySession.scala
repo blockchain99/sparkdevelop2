0d82e90b30f6ef8b2a73634db17e1f8de2f15367
blockchain99 <shinebytheriver@yahoo.com>
2017-12-05 18:29:44 -0800

    1

--- src/main/scala/com/sparkTutorial/pairRdd/groupbykey/GroupByKeyVsReduceByKeySession.scala
+++ src/main/scala/com/sparkTutorial/pairRdd/groupbykey/GroupByKeyVsReduceByKeySession.scala
@@ -11,7 +11,9 @@ object GroupByKeyVsReduceByKeySession {
   val words = List("one", "two", "two", "three", "three", "three")
   val wordsPairRdd = spark.sparkContext.parallelize(words).map(word => (word, 1))
 
-  val wordCountsWithReduceByKey = wordsPairRdd.reduceByKey((x, y) => x + y).collect()
+/*reduceByKey : preferred , which save shuffle across network.  */
+//  val wordCountsWithReduceByKey = wordsPairRdd.reduceByKey((x, y) => x + y).collect()  //manipulate values only
+  val wordCountsWithReduceByKey = wordsPairRdd.reduceByKey(_+_).collect()  //manipulate values only
   println("wordCountsWithReduceByKey: " + wordCountsWithReduceByKey.toList)
 
   val wordCountsWithGroupByKey = wordsPairRdd.groupByKey().mapValues(intIterable => intIterable.size).collect()
